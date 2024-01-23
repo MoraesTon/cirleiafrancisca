1f2db620719b414b4fce76d1fbc9448735428bed
beplusdigital <tondevspace@gmail.com>
2024-01-23 09:57:33 -0300

    new layout update

---
+++ js/coursesModule.js
@@ -0,0 +1,50 @@
+const courses = document.querySelector(".courses");
+
+courses.innerHTML = `
+<div class="courses-title">
+    <h2>Cursos</h2>
+ </div>
+<div class="courses-body">
+
+    <div class="card">
+
+        <svg class="bd-placeholder-img card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Image cap" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect><text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text></svg>
+
+        <h5 class="card-title card-header">Designer de sobrancelhas</h5>
+        <div class="card-body">
+        <h6 class="card-subtitle mb-2 text-body-secondary">Conquiste a sua liberdade financeira</h6>
+        <p class="card-text">Aprenda uma profissão rentável que te trará resultados com pouco investimento.</p>
+        <a href="#" class="btn btn-primary">Quero saber mais</a>
+        </div>
+    </div>
+  
+    <div class="card">
+
+        <svg class="bd-placeholder-img card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Image cap" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect><text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text></svg>
+
+        
+        <h5 class="card-title card-header">Estilista de Unhas</h5>
+        <div class="card-body">
+        <h6 class="card-subtitle mb-2 text-body-secondary">Conquiste a sua liberdade financeira</h6>
+        <p class="card-text">Aprenda uma profissão rentável que te trará resultados com pouco investimento.</p>
+        <a href="#" class="btn btn-primary">Quero saber mais</a>
+        </div>
+    </div>
+
+    <div class="card">
+
+        <svg class="bd-placeholder-img card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Image cap" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect><text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text></svg>
+
+        
+        <h5 class="card-title card-header">Experiência do Cliente</h5>
+        <div class="card-body">
+        <h6 class="card-subtitle mb-2 text-body-secondary">Conquiste a sua liberdade financeira</h6>
+        <p class="card-text">Saiba como criar um ambiente propício e engajar a sua clientela para gerar melhores resultados e melhorar as vendas e agendamentos.</p>
+        <a href="#" class="btn btn-primary">Quero saber mais</a>
+        </div>
+
+    </div>
+
+          
+</div>
+`;
